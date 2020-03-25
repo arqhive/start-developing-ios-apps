a69137b6934f02d579c3c9eaaf7b9ce55e4a07a5
wemeet-arqhive <arqhive@wemeetplace.com>
2020-03-25 14:32:51 +0900

    이미지 픽커를 이용해 라이브러리에서 이미지를 가져오고 UI에 표현

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -14,7 +14,7 @@ import UIKit
 // 뷰 컨트롤러가 뷰와 데이터 모델간의 통신 파이프 라인 역할하는 스타일의 앱 디자인을 MVC 패턴이라고 한다.
 // 이 패턴에서 모델은 앱의 데이터를 추적하고, 뷰는 사용자 인터페이스를 표시하고 구성하며 컨트롤러는 뷰를 핸들링 한다.
 // 사용자의 조치에 응답하고 데이터 모델의 컨텐츠로 뷰를 채우면 컨트롤러는 모델과 뷰간의 통신을 위한 게이트웨이 역할을 한다.
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     //MARK: Properties
     // IBOutlet 속성은 Interface Builder에서 nameTextField 프로퍼티에 연결 할 수 있음을 알려준다.
@@ -65,6 +65,27 @@ class ViewController: UIViewController, UITextFieldDelegate {
         mealNameLabel.text = "Default Text"
     }
     
+    // 사용자가 이미지를 누르면 사진 라이브러리에서 사진을 선택하거나 자신의 사진을 찍을 수 있어야 한다.
+    // 이는 UIImagePickerController 클래스에 동작이 내장되어 있다.
+    // 해당 컨트롤러를 사용하기 위해 UIImagePickerControllerDelegate 프로토콜을 채택한다.
+    // 기본 탐색을 위한 UINavigationControllerDelegate도 채택한다.
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        // 키보드가 열려있다면 닫는다.
+        nameTextField.resignFirstResponder()
+        // 컨트롤러 인스턴스 생성
+        let imagePickerController = UIImagePickerController()
+        // 사진 선택만 가능하다.
+        imagePickerController.sourceType = .photoLibrary // 열거형의 값이 이미 알려진 경우 언제든지 약식으로 쓸 수 있다.
+
+        // ViewController를 이미지픽커의 델리게이터로 만든다.
+        imagePickerController.delegate = self
+
+        // ViewController에서 호출되는 메서드 이다. 암시적인 자체 객체에서 실행된다.
+        // imagePickerController에 의해 정의된 뷰 컨트롤러를 표시하도록 요청한다.
+        // completion은 완료 핸드러를 나타내는데 여기서는 실행할 필요가 없으므로 nil로 설정한다.
+        present(imagePickerController, animated: true, completion: nil)
+    }
+
     // 텍스트필드에서 사용자 입력을 받아 들일 때 텍스트 필트 델리게이터의 도움이 필요하다.
     // 델리게이터는 대신 작용하고 따른 객체와의 통신을 가능하게하는 객체다.
     // 델리게이트 객채(이 경우 텍스트필드)는 다른 객체(델리게이터)에 대한 참조를 유지하며
@@ -98,6 +119,28 @@ class ViewController: UIViewController, UITextFieldDelegate {
     // 텍스트 필드에 입력한 정보를 읽고 무언가 실행 할 수 있다.
     func textFieldDidEndEditing(_ textField: UITextField) {
         mealNameLabel.text = textField.text
+    }
+    
+    //MARK: UIImagePickerControllerDelegate
+    // 사용자가 이미지 픽커의 취소 버튼을 탭하면 호출 된다. UIImagePickerController를 해제 할 수 있다.
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // 이미지 픽커를 사라지게 한다.
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // 사용자가 이미지를 선택하면 아래의 메서드가 실행된다.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // 파라미터 info 딕셔너리는 이미지의 여러 표현을 담고 있을 수 있다. 여기서는 originalImage를 사용한다.
+        // 선택된 이미지의 originalImage를 UIImage로 캐스트 한다.
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            fatalError("이미지가 포함된 딕셔너리를 예상했지만 다음과 같은 정보가 포함되어 있습니다.\(info)")
+        }
+        
+        // 이미지 뷰의 이미지를 교체
+        photoImageView.image = selectedImage
+        
+        // 픽커를 숨긴다.
+        dismiss(animated: true, completion: nil)
     }
     
 }
